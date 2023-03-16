205609246f8545f2afec1dc42a55d45543a67a28
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2023-03-15 19:43:37 -0600

    Add files via upload

---
+++ Codigo6/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GuardarButton = New Button()
+        CargarButton = New Button()
+        Lista = New TextBox()
+        ArchivoCargadoLabel = New Label()
+        ArchivoGuardadoLabel = New Label()
+        SuspendLayout()
+        ' 
+        ' GuardarButton
+        ' 
+        GuardarButton.Location = New Point(63, 314)
+        GuardarButton.Name = "GuardarButton"
+        GuardarButton.Size = New Size(94, 29)
+        GuardarButton.TabIndex = 0
+        GuardarButton.Text = "GUARDAD"
+        GuardarButton.UseVisualStyleBackColor = True
+        ' 
+        ' CargarButton
+        ' 
+        CargarButton.Location = New Point(480, 314)
+        CargarButton.Name = "CargarButton"
+        CargarButton.Size = New Size(94, 29)
+        CargarButton.TabIndex = 1
+        CargarButton.Text = "CARGAR"
+        CargarButton.UseVisualStyleBackColor = True
+        ' 
+        ' Lista
+        ' 
+        Lista.Location = New Point(263, 56)
+        Lista.Name = "Lista"
+        Lista.Size = New Size(125, 27)
+        Lista.TabIndex = 4
+        ' 
+        ' ArchivoCargadoLabel
+        ' 
+        ArchivoCargadoLabel.AutoSize = True
+        ArchivoCargadoLabel.Location = New Point(507, 248)
+        ArchivoCargadoLabel.Name = "ArchivoCargadoLabel"
+        ArchivoCargadoLabel.Size = New Size(53, 20)
+        ArchivoCargadoLabel.TabIndex = 3
+        ArchivoCargadoLabel.Text = "Label1"' 
+        ' ArchivoGuardadoLabel
+        ' 
+        ArchivoGuardadoLabel.AutoSize = True
+        ArchivoGuardadoLabel.Location = New Point(74, 221)
+        ArchivoGuardadoLabel.Name = "ArchivoGuardadoLabel"
+        ArchivoGuardadoLabel.Size = New Size(53, 20)
+        ArchivoGuardadoLabel.TabIndex = 2
+        ArchivoGuardadoLabel.Text = "Label1"' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Lista)
+        Controls.Add(ArchivoCargadoLabel)
+        Controls.Add(ArchivoGuardadoLabel)
+        Controls.Add(CargarButton)
+        Controls.Add(GuardarButton)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents GuardarButton As Button
+    Friend WithEvents CargarButton As Button
+    Friend WithEvents Lista As TextBox
+    Friend WithEvents ArchivoCargadoLabel As Label
+    Friend WithEvents ArchivoGuardadoLabel As Label
+End Class
---
+++ Codigo6/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+
+
+    Private Sub GuardarButton_Click_1(sender As Object, e As EventArgs) Handles GuardarButton.Click
+        Try
+            ' Abrir el archivo binario en modo de escritura
+            Using archivo As New FileStream("numeros.bin", FileMode.Create, FileAccess.Write)
+                ' Convertir la lista de números a bytes
+                Dim numeros As Byte() = System.Text.Encoding.UTF8.GetBytes(Lista.Text)
+                ' Escribir los bytes en el archivo
+                archivo.Write(numeros, 0, numeros.Length)
+                ' Mostrar un mensaje de éxito
+                ArchivoGuardadoLabel.Text = "Archivo guardado exitosamente."
+            End Using
+        Catch ex As Exception
+            ' Mostrar un mensaje de error
+            MessageBox.Show("Error al guardar el archivo: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub CargarButton_Click(sender As Object, e As EventArgs) Handles CargarButton.Click
+        Try
+            ' Abrir el archivo binario en modo de lectura
+            Using archivo As New FileStream("numeros.bin", FileMode.Open, FileAccess.Read)
+                ' Leer los bytes del archivo
+                Dim numeros As Byte() = New Byte(archivo.Length - 1) {}
+                archivo.Read(numeros, 0, numeros.Length)
+                ' Convertir los bytes a texto
+                Lista.Text = System.Text.Encoding.UTF8.GetString(numeros)
+                ' Mostrar un mensaje de éxito
+                ArchivoCargadoLabel.Text = "Archivo cargado exitosamente."
+            End Using
+        Catch ex As Exception
+            ' Mostrar un mensaje de error
+            MessageBox.Show("Error al cargar el archivo: " & ex.Message)
+        End Try
+    End Sub
+End Class
+
